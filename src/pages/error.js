21690656c868fd72b600f949c72f44594ce7f929
Jecoms <jessezao@gmail.com>
2019-10-22 19:08:33 -0500

    added error page

---
+++ src/pages/error.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const ErrorPage = () => (
+  <Layout>
+    <SEO title="Error" />
+    <h1>Error</h1>
+    <p>Something broke. Sorry 'bout that.</p>
+  </Layout>
+)
+
+export default ErrorPage
